d6fe476070534b8050c87e3ec55ce1f29bcad331
wangguangan <53215115+wga2016410498@users.noreply.github.com>
2024-04-18 00:21:15 +0800

    Delete SpringBoot directory

--- SpringBoot/src/main/java/com/example/mapper/PermissionMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import com.example.pojo.Permission;
-
-import java.util.Set;
-
-public interface PermissionMapper {
-    Set<Permission> SelectPermissionByRole(int role);
-}
